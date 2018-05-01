3d6d8ec225dbb77371a2b1280df920a71fb70065
Tahir Fazal <tahirawan4@gmail.com>
2018-05-01 15:27:55 +0500

    adding files.

---
+++ inference2/Proofs/start_and_stop.py
@@ -0,0 +1,24 @@
+
+def info():
+
+    order = [0, 0, 0, "#", 11, 14, 21, 71, 73, 79, 93, 178]
+    get_words_used = 0
+    proof_type = 0
+    # 194, 196, 180, 121, 109, 90, 24
+    # 0 is for testing the whole code without printing
+    # 1 is for printing all sentences to excel,
+    # 2 is for one sentence to terminal
+    # 3 is for testing spelling errors
+    # 4 ignores false sentences
+    # easy printing of definitions
+    return get_words_used, order
+
+
+
+# to prove all sentences press im 0 1
+
+
+# im 3 1, do not put a period before # do not put a period on the last sentence
+# separate the right sentences from the wrong sentences with #
+
+# to check mispellings put in im 4 0
